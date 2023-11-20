5b841bd9ea0ec5464f323535c475b3673db3a8d7
Andrew Eshak Wreford <46035160+wrefinity@users.noreply.github.com>
2023-11-20 01:13:34 +0100

    updating code behaviours

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ import numpy as np
 import streamlit as st
 from PIL import Image
 from db import ConnectDB
+import datetime
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 model = joblib.load('credit_fraud.pkl')
 
@@ -16,6 +18,29 @@ with st.columns(3)[1]:
 # Create Streamlit app
 st.title("Credit Card Fraud Detection Model")
 st.write('visit the dataset used: https://www.kaggle.com/datasets/mlg-ulb/creditcardfraud')
+
+
+def is_numeric(value):
+    """check if the value is numeric or not"""
+    try:
+        _ = float(value)
+        return True
+    except ValueError:
+        return False
+
+
+# scale using MinMaxScaler
+def scale_values(values):
+    ''' function to scale the values'''
+    scaler = StandardScaler()
+    scaled_values = scaler.fit_transform([values])
+    return scaled_values.flatten()
+
+usr = None
+
+# create the tables
+ConnectDB.create_user_table()
+ConnectDB.create_prediction_table()
 
 
 # side bar menu creations
@@ -41,129 +66,175 @@ elif side_choice == "Login":
         if result:
             st.sidebar.success(f"Logged-In as {username}")
             is_logged_in = True
-            menu = ['Prediction', 'Users']
-            side_choice = st.sidebar.selectbox("Task", menu)         
+            usr = username
+            menu = None
+            if usr == 'admin':
+                menu = ['Prediction', 'Users', 'Predicted']
+            else:
+                menu = ['Prediction',  'Predicted']
+            side_checker = st.sidebar.selectbox("Task", menu)
+            # st.sidebar.selectbox        
 
-            if st.sidebar.checkbox("Prediction"):
+            if side_checker == "Prediction":
 
+                # User Information
+                st.header("User Information")
+                user_xname = st.text_input("Full Name")
+                card_xnumber = st.text_input("Card Number")
+                expiration_xdate = st.date_input("Expiration Date", min_value=datetime.date.today())
+                cvvx = st.text_input("CVV")
+
+                # Submit Button
+                # if st.button("Submit Transaction"):
+
+                #     card_res = ConnectDB.check_user_card(
+                #         fullname=user_xname,
+                #         card_number=card_xnumber,
+                #         expiration_date=expiration_xdate,
+                #         cvv=cvvx
+                #     )
+
+                    # if card_res:
+                        # Transaction Information
+                st.header("Transaction Information")
                 st.write("Enter features from V1-V28, seperated by commas, to check if the \
             transaction is legitimate or fraudulent:")
                 
                 col1, col2, col3, col4, col5 = st.columns(5)
                 with col1:
-                    v1 = st.text_input("enter v1")
-                with col2:
-                    v2 = st.text_input("enter v2")
-                with col3:
-                    v3 = st.text_input("enter v3")
-                with col4:
                     v4 = st.text_input("enter v4")
-                with col5:
-                    v5 = st.text_input("enter v5")
-                # row two
-                with col1:
-                    v6 = st.text_input("enter v6")
                 with col2:
                     v7 = st.text_input("enter v7")
                 with col3:
-                    v8 = st.text_input("enter v8")
-                with col4:
                     v9 = st.text_input("enter v9")
-                with col5:
-                    v10 = st.text_input("enter v10")
-                # row three
-                with col1:
-                    v11 = st.text_input("enter v11")
-                with col2:
+                with col4:
                     v12 = st.text_input("enter v12")
-                with col3:
-                    v13 = st.text_input("enter v13")
-                with col4:
-                    v14 = st.text_input("enter v14")
                 with col5:
-                    v15 = st.text_input("enter v15")
-
-                # row four
-                with col1:
-                    v16 = st.text_input("enter v16")
-                with col2:
-                    v17 = st.text_input("enter v17")
-                with col3:
                     v18 = st.text_input("enter v18")
-                with col4:
+                # row two
+                with col1:
                     v19 = st.text_input("enter v19")
-                with col5:
+                with col2:
                     v20 = st.text_input("enter v20")
-
-                # row five
-                with col1:
+                with col3:
                     v21 = st.text_input("enter v21")
-                with col2:
-                    v22 = st.text_input("enter v22")
-                with col3:
-                    v23 = st.text_input("enter v23")
                 with col4:
-                    v24 = st.text_input("enter v24")
+                    v22 = st.text_input("enter v22")
                 with col5:
-                    v25 = st.text_input("enter v25")
-
-                # row six
-                with col1:
-                    v26 = st.text_input("enter v26")
-                with col2:
-                    v27 = st.text_input("enter v27")
-                with col3:
                     v28 = st.text_input("enter v28")
 
                 # Create input fields for the user to enter feature values
                 # input_df = st.text_input('V1,..V28 features')
                 input_df_lst = [
-                    v1, v2, v3, v4, v5, v6, v7, v8, v9, v10,
-                    v11, v12, v13, v14, v15, v16, v17, v18, v19, v20,
-                    v21, v22, v23, v24, v25, v26, v27, v28
+                    v4, v7, v9, v12, v18, v19, v20, v21, v22, v28,
                 ]
 
-                # Create a button to submit input and get a prediction
-                submit = st.button("Submit")
+                # Validation check
+                if all(is_numeric(value) for value in input_df_lst):
+                    submit_button = st.button("Submit")
+                    if submit_button:
+                        card_res = ConnectDB.check_user_card(
+                            fullname=user_xname,
+                            card_number=card_xnumber,
+                            expiration_date=expiration_xdate,
+                            cvv=cvvx
+                        )
 
-                if submit:
-                    # Get input feature values
-                    features = np.array(input_df_lst, dtype=np.float64)
+                        if not card_res:
+                            st.error("provide a valid credit or debit card details")
+                        else:
+                            # Get input feature values
+                            features = np.array(input_df_lst, dtype=np.float64)
+                            # Scale the input values
+                            # scaled_input = scale_values(input_df_lst)
 
-                    # Make prediction
-                    prediction = model.predict(features.reshape(1, -1))
+                            # Make prediction
+                            prediction = model.predict(features.reshape(1, -1))
+                            # prediction = model.predict(scaled_input)
 
-                    # Display the result
-                    if prediction[0] == 0:
-                        st.write("Legitimate transaction")
-                    else:
-                        st.write("Fraudulent transaction")
-
+                            # Display the result
+                            status = None
+                            if prediction[0] == 0:
+                                status = "Legitimate"
+                                st.write(f"The transaction details \
+                                        captured is {status}")
+                                ConnectDB.create_prediction(
+                                    username=username,
+                                    v4=v4, v7=v7,
+                                    v9=v9, v12=v12,
+                                    v18=v18, v19=v19,
+                                    v20=v20, v21=v21,
+                                    v22=v22, v28=v28,
+                                    status=status
+                                )
+                            else:
+                                status = "Fraudulent"
+                                st.write(f"the transaction detail given is a \
+                                        ${status} transaction")
+                                ConnectDB.create_prediction(
+                                    username=username,
+                                    v4=v4, v7=v7,
+                                    v9=v9, v12=v12,
+                                    v18=v18, v19=v19,
+                                    v20=v20, v21=v21,
+                                    v22=v22, v28=v28,
+                                    status=status
+                                )
+                    
+                else:
+                    st.warning("Please enter valid numeric values \
+                                for all transactions.")
+                    # else:
+                    #     st.error("invalid card credentials")
             # display users
-            if st.sidebar.checkbox("Users"):
+            if side_checker == "Users":
 
                 st.title('User Data Display')
                 # Fetch data from SQLite database
                 data = ConnectDB().get_user()
                 # Convert the fetched data to a Pandas DataFrame
-                df = pd.DataFrame(data, columns=['username'])
-
+                df = pd.DataFrame(data, columns=['username', 'fullname', 'card_number', 'expiration_date'])
                 # Display the DataFrame using Streamlit
-                st.dataframe(df, height=800, width=1200)
+                st.dataframe(df.dropna(), height=800, width=1200)
+            
+            if side_checker == "Predicted":
+                st.title('Predicted Data Display')
+                # Fetch data from SQLite database
+                data_x = ConnectDB().get_predictions()
+                # Convert the fetched data to a Pandas DataFrame
+                df = pd.DataFrame(data_x, columns=['id', 'username', 'v4', 'v7', 'v9', 'v12', 'v18', 'v19', 'v20', 'v21', 'v22', 'v28', 'status'])
+
+                # Filter the DataFrame based on the selected username
+                if usr != 'admin':
+                    filtered_df = df.loc[df['username'] == usr]
+                else:
+                    filtered_df = df.copy()
+                # Display the cleaned DataFrame using Streamlit
+                st.dataframe(filtered_df, height=800, width=1200)
+                # st.dataframe(df, height=800, width=1800)
+        
         else:
             st.sidebar.error("wrong credentials")
 
 elif side_choice == "SignUp":
     st.subheader("SignUp")
 
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
+    username_reg = st.text_input("Username")
+    password_reg = st.text_input("Password", type="password")
+    fullxx_name = st.text_input("Full Name", "")
+    card_num_reg = st.text_input("Card Number", "")
+    expiration_date_reg = st.date_input("Expiration Date", min_value=datetime.date.today())
+    cvv_reg = st.text_input("CVV", "")
 
     if st.button("SignUp"):
         ConnectDB.create_user_table()
         ConnectDB.create_user(
-            username=username,
-            password=password
+            username=username_reg,
+            password=password_reg,
+            fullname=fullxx_name,
+            card_number=card_num_reg,
+            expiration_date=expiration_date_reg,
+            cvv=cvv_reg
         )
         st.success("Account created")
         st.info("Goto Login")
--- db.py
+++ db.py
@@ -6,21 +6,79 @@ class ConnectDB:
 
     @staticmethod
     def create_user_table():
+        '''create user tables'''
         connection = sqlite3.connect('creditcard.db')
         cursor = connection.cursor()
         cursor.execute(
-            'CREATE TABLE IF NOT EXISTS user (username TEXT, password TEXT)'
+            'CREATE TABLE IF NOT EXISTS user ('
+            'id INTEGER PRIMARY KEY AUTOINCREMENT, '
+            'username TEXT, '
+            'password TEXT, '
+            'fullname TEXT, '
+            'card_number TEXT, '
+            'expiration_date TEXT, '
+            'cvv TEXT)'
+        )
+        connection.commit()
+        connection.close()
+    
+    @staticmethod
+    def create_prediction_table():
+        '''create prediction table'''
+        connection = sqlite3.connect('creditcard.db')
+        cursor = connection.cursor()
+        cursor.execute(
+            'CREATE TABLE IF NOT EXISTS prediction ('
+            'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+            'username TEXT,'
+            'v4 REAL,'
+            'v7 REAL,'
+            'v9 REAL,'
+            'v12 REAL,'
+            'v18 REAL,'
+            'v19 REAL,'
+            'v20 REAL,'
+            'v21 REAL,'
+            'v22 REAL,'
+            'v28 REAL,'
+            'status TEXT'
+            ')'
         )
         connection.commit()
         connection.close()
 
     @staticmethod
-    def create_user(username, password):
+    def create_user(
+        username,
+        password,
+        fullname,
+        card_number,
+        expiration_date,
+        cvv
+    ):
+        '''create users func'''
         connection = sqlite3.connect('creditcard.db')
         cursor = connection.cursor()
         cursor.execute(
-            'INSERT INTO user (username, password) VALUES (?, ?)',
-            (username, password)
+            'INSERT INTO user (username, password, fullname, card_number, \
+                expiration_date, cvv) VALUES (?, ?, ?, ?, ?, ?)',
+            (username, password, fullname, card_number, expiration_date, cvv)
+        )
+        connection.commit()
+        connection.close()
+    
+    @staticmethod
+    def create_prediction(
+        username,
+        v4, v7, v9, v12, v18, v19, v20, v21, v22, v28,
+        status
+    ):
+        '''create users func'''
+        connection = sqlite3.connect('creditcard.db')
+        cursor = connection.cursor()
+        cursor.execute(
+            'INSERT INTO prediction (username, v4, v7, v9, v12, v18, v19, v20, v21, v22, v28, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+            (username, v4, v7, v9, v12, v18, v19, v20, v21, v22, v28, status)
         )
         connection.commit()
         connection.close()
@@ -41,10 +99,47 @@ class ConnectDB:
         return user_data
 
     @staticmethod
-    def get_user():
+    def get_predictions(usr=None):
         connection = sqlite3.connect('creditcard.db')
         cursor = connection.cursor()
-        cursor.execute('SELECT username FROM user')
+        cursor.execute('SELECT * FROM prediction')
         data = cursor.fetchall()
         connection.close()
         return data
+    
+    @staticmethod
+    def get_user():
+        connection = sqlite3.connect('creditcard.db')
+        cursor = connection.cursor()
+        cursor.execute('SELECT username, fullname, card_number, \
+                       expiration_date FROM user')
+        data = cursor.fetchall()
+        connection.close()
+        return data
+    
+    @staticmethod
+    def check_user_card(
+        fullname,
+        card_number,
+        expiration_date,
+        cvv
+    ):
+        """check card details"""
+        # Connect to the database
+        connection = sqlite3.connect('creditcard.db')
+        cursor = connection.cursor()
+
+        # Execute the SELECT query
+        cursor.execute(
+            'SELECT * FROM user WHERE fullname = ? AND card_number = ? AND expiration_date = ? AND cvv = ?',
+            (fullname, card_number, expiration_date, cvv)
+        )
+
+        # Fetch the result
+        result = cursor.fetchone()
+
+        # Close the connection
+        connection.close()
+
+        # Return True if the result is not None, indicating the user exists
+        return result is not None
